4db32b6951a73829313686a8a8c3ac888a841727
Ilya Koloshin <ilya.koloshin@gmail.com>
2025-01-04 17:14:23 +0400

    add graph

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,20 @@
 use diesel::{
     migration::MigrationConnection, sqlite::SqliteConnection, Connection, ConnectionError,
-    ConnectionResult,
 };
+use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
+use rustworkx_core::petgraph::graph::DiGraph;
 
 pub mod models;
 pub mod schema;
 
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("migrations");
+
 pub struct Error {
     text: String,
+}
+
+pub struct GraphConnection {
+    connection: SqliteConnection,
 }
 
 impl From<ConnectionError> for Error {
@@ -26,14 +33,29 @@ impl From<diesel::result::Error> for Error {
     }
 }
 
+impl From<Box<dyn std::error::Error + Send + Sync>> for Error {
+    fn from(value: Box<dyn std::error::Error + Send + Sync>) -> Self {
+        Error {
+            text: format!("{value}"),
+        }
+    }
+}
+
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.write_str(&self.text)
     }
 }
 
-pub fn connect(database_url: &str) -> Result<(), Error> {
+impl GraphConnection {
+    pub fn save(self, graph: DiGraph<String, String>) {}
+}
+
+pub fn connect(database_url: &str) -> Result<GraphConnection, Error> {
     let mut connection = SqliteConnection::establish(database_url)?;
     connection.setup()?;
-    Ok(())
+    connection.run_pending_migrations(MIGRATIONS)?;
+    Ok(GraphConnection {
+        connection: connection,
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use rustworkx_core_sql_serialization::{self as lib, connect};
+use diesel::connection;
+use rustworkx_core::petgraph::prelude::DiGraph;
+use rustworkx_core_sql_serialization::{self as lib, connect, schema::edges::payload};
 
 fn main() {
     let dbpath = match std::env::var("DBPATH") {
@@ -8,11 +10,17 @@ fn main() {
             return;
         }
     };
-    match connect(&dbpath) {
-        Ok(_val) => println!("Connected"),
+
+    let connection = match connect(&dbpath) {
+        Ok(val) => val,
         Err(e) => {
             println!("couldn't interpret {e}");
             return;
         }
-    }
+    };
+    let mut graph = DiGraph::<String, String>::new();
+    let node1 = graph.add_node("aaa".to_string());
+    let node2 = graph.add_node("bbb".to_string());
+    graph.add_edge(node1, node2, "payload".to_string());
+    connection.save(graph);
 }
