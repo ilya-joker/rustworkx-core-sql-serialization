5f3d1a51ee78009465f8a4688b87441f8502d062
Ilya Koloshin <ilya.koloshin@gmail.com>
2025-01-04 15:34:31 +0400

    add Error and setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,36 @@
-use diesel::{sqlite::SqliteConnection, Connection, ConnectionResult};
+use diesel::{
+    migration::MigrationConnection, sqlite::SqliteConnection, Connection, ConnectionError,
+    ConnectionResult,
+};
 
-pub fn connect(database_url: &str) -> ConnectionResult<()> {
-    SqliteConnection::establish(database_url).map(|_| ())
+pub struct Error {
+    text: String,
+}
+
+impl From<ConnectionError> for Error {
+    fn from(value: ConnectionError) -> Self {
+        Error {
+            text: format!("{value}"),
+        }
+    }
+}
+
+impl From<diesel::result::Error> for Error {
+    fn from(value: diesel::result::Error) -> Self {
+        Error {
+            text: format!("{value}"),
+        }
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.text)
+    }
+}
+
+pub fn connect(database_url: &str) -> Result<(), Error> {
+    let mut connection = SqliteConnection::establish(database_url)?;
+    connection.setup()?;
+    Ok(())
 }
